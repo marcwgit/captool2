dc9a09c87b38a9300bf038dc64c75b6b63d447cb
marcwgit <marcwgit@gmail.com>
2018-05-02 12:48:16 -0400

    hms format

--- subedit/assmbl2.sh
+++ subedit/assmbl2.sh
@@ -18,6 +18,29 @@ captype=".srt"
 j="j"
 
 
+convertAndPrintSeconds() {
+ 
+    local totalSeconds=$1;
+    local seconds=$((totalSeconds%60));
+    local minutes=$((totalSeconds/60%60));
+    local hours=$((totalSeconds/60/60%24));
+    local days=$((totalSeconds/60/60/24));
+    (( $days > 0 )) && printf '%d days ' $days;
+    (( $hours > 0 )) && printf '%d hours ' $hours;
+    (( $minutes > 0 )) && printf '%d minutes ' $minutes;
+    (( $days > 0 || $hours > 0 || $minutes > 0 )) && printf 'and ';
+    printf '%d seconds\n' $seconds;
+}
+
+#function hms(s)
+#{
+#  h=int(s/3600);
+#  s=s-(h*3600);
+#  m=int(s/60);
+#  s=s-(m*60);
+#  printf("%d:%02d:%02d\n", h, m, s);
+#}
+
 ##    echo filename =~ *[0-9,0-9]*.srt
 #[$filename =~ $regex]
 #maxfile="${BASH_REMATCH[1]}"
@@ -72,11 +95,25 @@ do
 	### original	ffprobe -i bettina--01.mp3  -v quiet -show_format -sexagesimal | sed -n 's/duration=//p';
 		
 #		hms= "ffprobe -i "$audiofile"#  -v quiet -show_format -sexagesimal" # | "sed -n 's/duration=//p'"
+		sec=$(ffprobe -i $audiofile  -v quiet -show_format | sed -n 's/duration=//p')
 		hms=$(ffprobe -i $audiofile  -v quiet -show_format -sexagesimal | sed -n 's/duration=//p')
+		
+		
 ####	/home/marcwe/subedit/subedit -i file name -s +hms
 ###	cat file >> out.srt
 
+	echo $sec
 	echo $hms
+	echo $sec | awk {'h=int($0/3600);r=($0-(h*3600));m=int(r/60);s=(r-(m*60)); print h ":" m ":" s'}
+	echo $sec | awk {'h=int($0/3600);r=($0-(h*3600));m=int(r/60);s=(r-(m*60)); printf "%02d" ":" "%02d" ":" "%02.3f",h, m, s'}
+	convhmsf=$(echo $sec | awk {'h=int($0/3600);r=($0-(h*3600));m=int(r/60);s=(r-(m*60)); printf "%02d" ":" "%02d" ":" "%02.3f",h, m, s'})
+	convhms=$(echo $sec | awk {'h=int($0/3600);r=($0-(h*3600));m=int(r/60);s=(r-(m*60)); print h ":" m ":" s'})
+	echo $convhms
+	echo $convhmsf
+	#$convhms=$(echo ${convhms//
+	
+#	echo convertAndPrintSeconds(${sec})
+#	echo hms($sec)
 	echo $i
 done
 
